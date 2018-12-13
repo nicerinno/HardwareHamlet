d5adeed0901b10900d9fb447395d49c4c7256983
Rodrigo Serra <rodrigoserrafernandes98@gmail.com>
2018-12-13 18:23:07 +0000

    Update Base de dados/BDSQL_code.sql

--- Base de dados/BDSQL_code.sql
+++ Base de dados/BDSQL_code.sql
@@ -1,35 +1,36 @@
 CREATE DATABASE HardwareHamlet;
 
 CREATE TABLE medals(
-    medal_id int not null PRIMARY KEY,
+    medal_id int not null PRIMARY KEY AUTO_INCREMENT,
     name varchar(100) not null,
     icon varchar(100) not null,
     amount_likes int not null
 );
 
 CREATE TABLE titles(
-    title_id int not null PRIMARY KEY,
+    title_id int not null PRIMARY KEY AUTO_INCREMENT,
     name varchar(100) not null,
-    rep_amount int not null
+    rep_amount int not null,
+	color varchar(50) not null
 );
 
 CREATE TABLE user_type(
-    usertype_id int not null PRIMARY KEY,
+    usertype_id int not null PRIMARY KEY AUTO_INCREMENT,
     name varchar(100) not null
 );
 
 CREATE TABLE component_type(
-    component_type_id int not null PRIMARY KEY,
+    component_type_id int not null PRIMARY KEY AUTO_INCREMENT,
     name varchar(100) not null
 );
 
 CREATE TABLE build_type(
-    buildtype_id int not null PRIMARY KEY,
+    buildtype_id int not null PRIMARY KEY AUTO_INCREMENT,
     name varchar(100) not null
 );
 
 CREATE TABLE users(
-    user_id int not null PRIMARY KEY,
+    user_id int not null PRIMARY KEY AUTO_INCREMENT,
     title_id int not null,
     usertype_id int not null,
     medal_id int not null,
@@ -38,26 +39,29 @@ CREATE TABLE users(
     password varchar(100) not null,
     description varchar(500) not null,
     reputation int not null,
+	active boolean not null,
+	validation_code varchar(12) not null,
     FOREIGN KEY (title_id) REFERENCES titles(title_id),
     FOREIGN KEY (usertype_id) REFERENCES user_type(usertype_id),
     FOREIGN KEY (medal_id) REFERENCES medals(medal_id)
 );
 
 CREATE TABLE components(
-    component_id int not null PRIMARY KEY,
+    component_id int not null PRIMARY KEY AUTO_INCREMENT,
     component_type_id int not null,
     user_id int not null,
     brand varchar(100) not null,
     name varchar(150) not null,
-    description varchar(500) not null,
-    price varchar(50) not null,
+    description varchar(2000) not null,
+    price double not null,
     flg_available boolean not null,
+	icon_url varchar(200) not null,
     FOREIGN KEY (component_type_id) REFERENCES component_type(component_type_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE builds(
-    build_id int not null PRIMARY KEY,
+    build_id int not null PRIMARY KEY AUTO_INCREMENT,
     user_id int not null,
     buildtype_id int not null,
     name varchar(100) not null,
@@ -79,7 +83,7 @@ CREATE TABLE build_components(
 );
 
 CREATE TABLE comments(
-    comment_id int not null PRIMARY KEY,
+    comment_id int not null PRIMARY KEY AUTO_INCREMENT,
     build_id int not null,
     content varchar(500) not null,
     FOREIGN KEY (build_id) REFERENCES builds(build_id)
