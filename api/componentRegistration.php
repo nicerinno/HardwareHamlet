71bea26edc93a1f1c6e5386dcb51e10a7f00f1d0
Rodrigo Serra <rodrigoserrafernandes98@gmail.com>
2019-01-07 17:56:21 +0000

    More user stuff

--- api/componentRegistration.php
+++ api/componentRegistration.php
@@ -19,39 +19,50 @@ if(!empty($input->component_type_id) && !empty($input->user_id) && !empty($input
     && !empty($input->name) && !empty($input->description) && !empty($input->price)
     && !empty($input->flg_available) && !empty($input->icon_url)){
 
-    //inserting the component in Database
-    $sql = "INSERT INTO components VALUES('','$input->component_type_id','$input->user_id','$input->brand','$input->name','$input->description','$input->price','$input->flg_available','$input->icon_url')";
-    $regist = $conn->query($sql);
-    if($regist){
+    $checkIfActive = "SELECT * FROM users WHERE user_id='$input->build_name' AND active=true";
+    $runCheckActive = $conn->query($checkIfActive);
 
-        $sql2 = "UPDATE users SET reputation = reputation + 25 WHERE user_id = '$input->user_id'";
-        $setRep = $conn->query($sql2);
+    if($runCheckActive->num_rows > 0){
+//inserting the component in Database
+        $sql = "INSERT INTO components VALUES('','$input->component_type_id','$input->user_id','$input->brand','$input->name','$input->description','$input->price','$input->flg_available','$input->icon_url')";
+        $regist = $conn->query($sql);
+        if($regist){
 
-        //check if user is eligible for new Title
-        //getting the amount of rep the user has
-        $getRepNumber = "SELECT reputation FROM users WHERE user_id = '$input->user_id' ";
-        $queryRep = $conn->query($getRepNumber)->fetch_assoc();
-        $repAmount = $queryRep['reputation'];
+            $sql2 = "UPDATE users SET reputation = reputation + 25 WHERE user_id = '$input->user_id'";
+            $setRep = $conn->query($sql2);
 
-        //checking if there is a title with that amount of rep
-        $checkForTitle = "SELECT title_id FROM titles WHERE rep_amount = '$repAmount'";
-        $queryCheck = $conn->query($checkForTitle);
-        $titleCheck = $queryCheck->fetch_assoc();
-        $title_id = $titleCheck['title_id'];
+            //check if user is eligible for new Title
+            //getting the amount of rep the user has
+            $getRepNumber = "SELECT reputation FROM users WHERE user_id = '$input->user_id' ";
+            $queryRep = $conn->query($getRepNumber)->fetch_assoc();
+            $repAmount = $queryRep['reputation'];
 
-        //if the check query returns a row then update the title
-        if($queryCheck->num_rows > 0){
-            $setNewTitle = "UPDATE users SET title_id = '$title_id' WHERE user_id = '$input->user_id'";
-            $queryNewTitel = $conn->query($setNewTitle);
+            //checking if there is a title with that amount of rep
+            $checkForTitle = "SELECT title_id FROM titles WHERE rep_amount = '$repAmount'";
+            $queryCheck = $conn->query($checkForTitle);
+            $titleCheck = $queryCheck->fetch_assoc();
+            $title_id = $titleCheck['title_id'];
+
+            //if the check query returns a row then update the title
+            if($queryCheck->num_rows > 0){
+                $setNewTitle = "UPDATE users SET title_id = '$title_id' WHERE user_id = '$input->user_id'";
+                $queryNewTitel = $conn->query($setNewTitle);
+            }
+
+            //json response body success
+            $data = ["request_type" => "component registration", "result" => "successfull"];
+            $component = new Component('',$input->component_type_id,$input->user_id,$input->brand,$input->name,$input->description,$input->price,$input->flg_available,$input->icon_url);
+        } else{
+            //json response body failure
+            $data = ["request_type" => "component registration", "result" => "failure " ];
         }
-
-        //json response body success
-        $data = ["request_type" => "component registration", "result" => "successfull"];
-        $component = new Component('',$input->component_type_id,$input->user_id,$input->brand,$input->name,$input->description,$input->price,$input->flg_available,$input->icon_url);
-    } else{
+    }else{
         //json response body failure
-        $data = ["request_type" => "component registration", "result" => "failure " ];
+        $data = ["request_type" => "build registration", "result" => "User not active"];
     }
+
+
+
     endConnDB($conn);
 }
 echo json_encode($data);
