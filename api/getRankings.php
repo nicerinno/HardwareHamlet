30024270cc4bfd6610d3b75d080d91036e11a20a
Rodrigo Serra <rodrigoserrafernandes98@gmail.com>
2019-01-25 01:41:44 +0000

    Update getRankings.php

--- api/getRankings.php
+++ api/getRankings.php
@@ -8,6 +8,8 @@
 include_once (__DIR__ . "/model/Users.php");
 include_once (__DIR__ . "/model/Component.php");
 include_once (__DIR__ . "/model/Builds.php");
+include_once (__DIR__ . "/model/Build_Components.php");
+include_once (__DIR__ . "/model/Comments.php");
 
 header("Content-Type: application/json");
 
@@ -31,15 +33,29 @@ if(isset($_GET['ranking'])){
             }
         }
     }else if($typeOfRanking == "components"){
-        $sql = "SELECT * FROM components order by price desc limit 5";
+        $sql = "select component_id, count(component_id) as count FROM build_components GROUP BY component_id order by count(component_id) desc limit 5";
+        $query = $conn->query($sql);
+        if($query->num_rows > 0){
+            while($row = $query->fetch_assoc()) {
+                $id = $row['component_id'];
+                $sql2 = "SELECT * FROM components WHERE component_id = '$id'";
+                $component = $conn->query($sql2)->fetch_assoc();
+                $newComponent = new Component($component['component_id'], $component['component_type_id'], $component['user_id'], $component['brand']
+                    , $component['name'], $component['description'], $component['price'], $component['flg_available'], $component['icon_url'], $component['regist_date']);
+
+                array_push($data, $newComponent);
+            }
+        }
+    }else if($typeOfRanking == "builds"){
+        $sql = "SELECT * FROM builds order by likes desc limit 5";
         $query = $conn->query($sql);
 
         if($query->num_rows > 0){
             while($row = $query->fetch_assoc()) {
-                $newComponent = new Component($row['component_id'], $row['component_type_id'], $row['user_id'], $row['brand']
-                    , $row['name'], $row['description'], $row['price'], $row['flg_available'], $row['icon_url'], $row['regist_date']);
+                $newBuild = new Builds($row['build_id'], $row['user_id'], $row['build_type_id'], $row['build_name']
+                    , $row['description'], $row['price'], $row['likes'], $row['regist_date']);
 
-                array_push($data, $newComponent);
+                array_push($data, $newBuild);
             }
         }
     }
